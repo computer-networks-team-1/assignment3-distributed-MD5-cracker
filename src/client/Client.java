c29573bb913f0059540fe87dce516c3a03b1a1c0
MichaelVT <mvontroyer@unibz.it>
2022-12-05 16:36:52 +0100

    Implemented master, slave interfaces, ecc

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,16 +17,27 @@ public class Client implements MasterCommInterface, SlaveCommInterface {
 
 	public static void main(String[] args) throws Exception {
 
-		String type = args[3];
+		if (args.length < 4) {
+			System.out.println("Proper Usage is: java Client serverAddress yourOwnIPAddress teamname type");
+			System.exit(0);
+		}
 
-		if (type.equals("master"))
-		{
+		String type = args[3];
+		if (
+			!(type.equalsIgnoreCase("slave") ||
+			type.equalsIgnoreCase("master"))
+			) {
+				System.out.println("type has to be either 'master' or 'slave'");
+				System.exit(0);
+		}
+
+		// This is crucial, otherwise the RPis will not be reachable from the server/master.
+		System.setProperty("java.rmi.server.hostname", args[1]);
+
+		if (type.equalsIgnoreCase("master")) {
 			String teamName = args[2];
 
 			System.out.println("Client starting, listens on IP " + args[1] + " for server callback.");
-
-			// This is crucial, otherwise the RPis will not be reachable from the server.
-			System.setProperty("java.rmi.server.hostname", args[1]);
 
 			// Initially we have no problem :)
 			byte[] problem = null;
@@ -38,15 +50,21 @@ public class Client implements MasterCommInterface, SlaveCommInterface {
 			ClientCommHandler cch = new ClientCommHandler();
 			System.out.println("Client registers with the server");
 			sci.register(teamName, cch);
-		} else {
-			MasterCommInterface sci = (MasterCommInterface) Naming.lookup("rmi://" + args[0] + "/server");
-			sci.subscribe(args[1]);
+
+			// Now forever solve tasks given by the server
+			while (true) {
+				// Wait until getting a problem from the server
+				while (cch.currProblem==null) {
+//				Thread.sleep(1);
+				}
+			}
+
+
 		}
 
-		if(args.length < 3)
-		{
-			System.out.println("Proper Usage is: java Client serverAddress yourOwnIPAddress teamname");
-			System.exit(0);
+		if (type.equalsIgnoreCase("slave")) {
+			MasterCommInterface mci = (MasterCommInterface) Naming.lookup("rmi://" + args[0] + "/server");
+			mci.subscribe(args[1]);
 		}
 
 		
@@ -57,12 +75,7 @@ public class Client implements MasterCommInterface, SlaveCommInterface {
 		//which value should the client start to compute from
 		int startingPoint = 100;
 		
-		// Now forever solve tasks given by the server
-		while (true) {
-			// Wait until getting a problem from the server
-			while (cch.currProblem==null) {
-//				Thread.sleep(1);
-			}
+
 
 			problem = cch.currProblem;
 			// Then bruteforce try all integers till problemsize
@@ -78,7 +91,7 @@ public class Client implements MasterCommInterface, SlaveCommInterface {
 					if (Arrays.equals(currentHash, problem)) {
 						System.out.println("client submits solution");
 						sci.submitSolution(teamName, i.toString());
-						cch.currProblem=null;
+						cch.currProblem = null;
 						break;
 					}
 				}
